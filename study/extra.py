2c834b01d53a35b14bcb271099a55a5adecbbb19
kkangwook <send2woohyun@naver.com>
2025-04-18 16:25:46 +0900

    Update extra.py

--- study/extra.py
+++ study/extra.py
@@ -14,4 +14,36 @@
 
 4. 변수가 그리 많지 않고 변수간 상관관계가 낮으며 과대적합이 걱정되지 않을때->그냥 선형회귀사용
    과대적합, 변수 많음, 다중공선성이 있으면 -> 릿지/라쏘 회귀 사용
-  
+
+
+#사용법
+
+#공통
+df  ->  to_numpy()  ->  x,y분리  ->  train_test_split으로 x_train, x_test, y_train, y_test로 분리
+->  변수 하나면 x.reshape(-1,1), 아니면 x는 이미 2차원 배열 (y는 그대로)
+
+#kneighborsregressor
+from sklearn.neighbors import KNeighborsRegressor
+-> standardscaler로 .fit(x_train)  ->  .transform(x_train), .transform(x_test)   (y는 안해도 됨)  
+->  knr=KNeighborsRegressor(n_neighbors=n)  ->  knr.fit(x_train,y_train)  ->knr.score로 과대/과소적합 판단  
+->  k개수 변경  ->knr.predict(2차원배열)  ->  dist,index=knr.kneighbors(2차원배열)  
+-> plt.plot(x_train[index],y_train[index])가 참고한 이웃
+
+# linear regression(1차 or 다항회귀)
+from sklearn.linear_model import LinearRegression
+-> 1차면 그대로/ 2차하고싶으면 x_train**2, x_test**2를 np.concatenate  ->  ss로 .fit(x_train)
+->  .transform(x_train), .transform(x_test)  ->  lr=LinearRegression()  ->  lr.fit(x_train,y_train)
+->  lr.score 확인  ->  lr.coef_, lr.intercept_확인  ->  그래프그리기  ->  range=np.arange(0,55)
+->  plt.plot(range,coef1*range**2+coef2*range+intercept)  ->  lr.predict(data,data**2)
+
+#다중회귀
+from sklearn.preprocessing import PolynomialFeatures
+->  poly=PolynomialFeatures(degree=n)  -> poly.fit(x_train)  ->  poly.transform(x_train), poly.transform(x_test)
+->  poly.get_feature_names_out()으로 어떻게 증가했는지 확인  ->   ss.fit(x_train)  ->  x_train,x_test을 .transform
+->  lr.fit  ->   lr.score  ->  lr.predict(poly_data)
+
+from sklearn.linear_model import Ridge, Lasso
+->  alpha_list=[0.001,0.01,0.1,1,10,100]로 for i in alpha_list: Ridge/Lasso(alpha=alpha)
+->  .fit(x_train,y_train)  ->  train_score/test_score.append(.score(train/test))
+->  plt.plot(np.log10(alpha_list),train_score/test_score) 그리고 제일 가까운 값 확인
+-> 그 alpha로 .fit  ->np.sum(.coef_==0)로 몇개 차원 없어졌는지 확인  ->.score  ->.predict(poly+ss_data)
