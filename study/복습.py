5cd5aeb64526c2740c189b226a324c593a5afc56
kkangwook <send2woohyun@naver.com>
2025-04-22 14:54:14 +0900

    Create 복습.py

---
+++ study/복습.py
@@ -0,0 +1,10 @@
+#1. linear regression
+
+주피터->machine_learining->복습으로
+1.  K-최근접 이웃회귀
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family'] = 'Malgun Gothic' 
+matplotlib.rcParams['font.size'] = 15 
+matplotlib.rcParams['axes.unicode_minus'] = False
+from sklearn.neighbors import KNeighborsRegressor
