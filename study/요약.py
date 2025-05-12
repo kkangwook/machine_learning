a4ff0d07149c1c948c7e9c08c657a03c256adc5c
kkangwook <send2woohyun@naver.com>
2025-05-12 11:12:48 +0900

    Create 요약.py

---
+++ study/요약.py
@@ -0,0 +1,7 @@
+1. 지도학습: x,y
+  --회귀: 연속숫자형x와 연속숫자형 y, 손실함수: MSE(mean squared error)- np.mean((y-y_pred)**2)
+  from sklearn.linear_model import LinearRegression
+  --분류
+2. 비지도학습: only x
+  --군집화 
+3. 강화학습 
