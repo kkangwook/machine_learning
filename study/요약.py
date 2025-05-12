1d20095e30a58834a9720017e20eac4702035cee
kkangwook <send2woohyun@naver.com>
2025-05-12 12:30:00 +0900

    Update 요약.py

--- study/요약.py
+++ study/요약.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 1. 지도학습: x,y
   ---#회귀: 연속숫자형x와 연속숫자형 y, 
+      #출력함수 따로 없음
   --손실함수: MSE(mean squared error)- np.mean((y-y_pred)**2)
      from sklearn.metrics import mean_squared_error as mse -> mse(y_true,y_pred) 
      or .score(x,y)로 R^2계수
@@ -33,8 +34,12 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
   ---#분류: x는 연속숫자 및 원핫인코딩-레이블인코딩-bow-tfidf전부 가능
-           #y클래스는 정수(레이블인코딩), 문자열, [T/F], [[1, 0], [0, 1]]과 같은 중복클래스를 같는 다중레이블 가능   
-
+        # y클래스는 정수(레이블인코딩), 문자열, [T/F], [[1, 0, 1], [0, 1, 1]]과 같은 중복클래스를 같는 다중레이블 가능   
+        # 출력함수: 이진분류=시그모이드(scipy.special.expit), 다중분류=softmax(scipy.special.softmax)
+        #손실함수: 이진=log_loss(cross-entropy), 다중= Categorical Cross Entropy
+        from sklearn.metrics import log_loss
+          log_loss(y_true, y_pred) #알아서 이진인지 다중인지 인식
+    -- 
 2. 비지도학습: only x
   --군집화 
 3. 강화학습 
