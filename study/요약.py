c19dc9e732f46458bab413008568879d6acef630
kkangwook <send2woohyun@naver.com>
2025-09-15 00:34:47 +0900

    Update 요약.py

--- study/요약.py
+++ study/요약.py
@@ -187,7 +187,7 @@ def tokenizer(x):
     -- 1-2-4 from sklearn.naive_bayes import MultinomialNB
         텍스트 용도로 사용!!!!!-> x는 tfidf형태로 들어감!!!!!(연속형 데이터는 GaussianNB사용-몰라도됌) 
         params = {'alpha': loguniform(1e-3, 1e3),  # alpha는 0.001과 1000 사이에서 loguniform 분포
-                  'fit_prior': bernoulli(0.5)}  # fit_prior는 True/False를 균등한 확률로 샘플링
+                  'fit_prior': [True, False]}  # fit_prior는 True/False를 균등한 확률로 샘플링
         - nb.predict_proba(x_test), nb.classes_, nb.coef_
 
 
